3c8e26e6a531e80902bba06cd8a8742db5837eca
slahirucd7 <slahiruc007@gmail.com>
2021-05-19 14:17:11 +0530

    project initialization

---
+++ src/main/java/pkg1/MainClass.java
@@ -0,0 +1,7 @@
+package pkg1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World 2!!!");
+    }
+}
---
+++ src/main/java/pkg1/TestClass.java
@@ -0,0 +1,9 @@
+package pkg1;
+
+public class TestClass {
+
+    public TestClass() {
+        System.out.println("Object is created !");
+        System.out.println("Object is created ... Execution going to start!");
+    }
+}
