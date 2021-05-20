a4aca61854d75d7c0e2e41f16089d61a8df7ed07
slahirucd7 <slahiruc007@gmail.com>
2021-05-20 22:22:57 +0530

    c9

--- src/main/java/pkg1/TestClass.java
+++ src/main/java/pkg1/TestClass.java
@@ -2,7 +2,7 @@ package pkg1;
 
 public class TestClass {
 
-    public TestClass(int a) {
+    public TestClass(int a , int b) {
         System.out.println("Object is created !");
         System.out.println("Object is created ... Execution going to start! 2....");
     }
