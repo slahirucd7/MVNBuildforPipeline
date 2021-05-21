daefa1d455b71168fd0080f9020e197f73d72785
slahirucd7 <slahiruc007@gmail.com>
2021-05-21 14:58:03 +0530

    pipeline file change3

--- src/main/java/pkg1/TestClass.java
+++ src/main/java/pkg1/TestClass.java
@@ -7,6 +7,6 @@ public class TestClass {
         System.out.println("Object is created ... Execution going to start! 2,3");
         System.out.println("This is a change in testBranch");
         System.out.println("This is a change 2 in the testBranch......1...2....3...4...5...");
-        System.out.println("This is a change a change made to trigger PR2  ");
+        System.out.println("This is a change a change....");
     }
 }
