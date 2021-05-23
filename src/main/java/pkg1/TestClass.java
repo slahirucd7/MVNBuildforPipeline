9024a20da9da29598ab3542a71eddd6f77f755c9
slahirucd7 <slahiruc007@gmail.com>
2021-05-23 16:54:25 +0530

    Test22

--- src/main/java/pkg1/TestClass.java
+++ src/main/java/pkg1/TestClass.java
@@ -12,6 +12,6 @@ public class TestClass {
     }
 
     public void TestMethod(){
-        System.out.println("This is a test method.. ... ...>>>>> ////////////    ");
+        System.out.println("This is a test method.. ... ...>>>>> /////////////    ");
     }
 }
